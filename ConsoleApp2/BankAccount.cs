ddd1c3a7148b025012fc5acedf54731b185dd64a
OneSAHDEVSINH <sahdevsinhparmar1753@gmail.com>
2025-04-24 16:00:39 +0530

    Add project files.

---
+++ ConsoleApp2/BankAccount.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class BankAccount
+    {
+        public string AccountNumber { get; set; }
+        public string AccountHolderName { get; set; }
+        public decimal Balance { get; set; }
+
+        public BankAccount(string accountNumber, string accountHolderName, decimal initialBalance = 0)
+        {
+            AccountNumber = accountNumber;
+            AccountHolderName = accountHolderName;
+            Balance = initialBalance;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            if (amount > 0)
+            {
+                Balance += amount;
+                Console.WriteLine($"Deposited {amount}. New balance is {Balance}.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid deposit amount. Please enter a positive number.");
+            }
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount > 0 && amount <= Balance)
+            {
+                Balance -= amount;
+                Console.WriteLine($"Withdrew {amount}. New balance is {Balance}.");
+            }
+            else if (amount > Balance)
+            {
+                Console.WriteLine("Insufficient funds for withdrawal.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid withdrawal amount. Please enter a positive number.");
+            }
+        }
+
+        public void CheckBalance()
+        {
+            Console.WriteLine($"Current balance is {Balance}.");
+        }
+    }
+}
